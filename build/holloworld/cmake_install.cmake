66eec7b64ccb06498ded5824b73d8c823379502e
feilong <614183595@qq.com>
2017-06-11 13:41:59 +0800

    first

---
+++ build/holloworld/cmake_install.cmake
@@ -0,0 +1,49 @@
+# Install script for directory: /home/pi/catkin_workspace/src/holloworld
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/home/pi/catkin_workspace/install")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "/home/pi/catkin_workspace/build/holloworld/catkin_generated/installspace/holloworld.pc")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/holloworld/cmake" TYPE FILE FILES
+    "/home/pi/catkin_workspace/build/holloworld/catkin_generated/installspace/holloworldConfig.cmake"
+    "/home/pi/catkin_workspace/build/holloworld/catkin_generated/installspace/holloworldConfig-version.cmake"
+    )
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/holloworld" TYPE FILE FILES "/home/pi/catkin_workspace/src/holloworld/package.xml")
+endif()
+
---
+++ build/mavpi/CMakeFiles/mavpi_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mavpi_node.dir/src/offb_node.cpp.o"
+  "/home/pi/catkin_workspace/devel/lib/mavpi/mavpi_node.pdb"
+  "/home/pi/catkin_workspace/devel/lib/mavpi/mavpi_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mavpi_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
