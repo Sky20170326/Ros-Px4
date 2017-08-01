5b3f27210bf577511b5f572a4eab00ffba4e5c54
feilong <614183595@qq.com>
2017-08-01 13:36:03 +0800

    增加协议内容
    增加协议内容

--- src/mavpi/src/protocol.h
+++ src/mavpi/src/protocol.h
@@ -10,13 +10,32 @@ extern "C" {
 #include <string.h>
 
 #define DIV 100
-#define BufferLength 50
+#define BufferLength 100
 #define splitChar ','
+
+enum planArmMode {
+    NoArm,
+    Arm
+};
+
+enum planeFlyMode {
+    Stable,
+    Alt,
+    Pos,
+    OffBoard
+};
+
+enum CtrlMode {
+    Pose,
+    Raw
+};
 
 typedef struct
 {
     int head;
     int index;
+    int ctrlMode;
+    int pitch, roll;
     int x, y, z, yaw;
     int div;
     int sumcheck;
@@ -28,10 +47,13 @@ typedef struct
 {
     int head;
     int index;
+    int armed;
+    int flyMode;
     int x, y, z, yaw;
     int x_set, y_set, z_set, yaw_set;
     int pitch;
     int roll;
+    int pitch_set, roll_set;
     int dis;
     int div;
     int sumcheck;
@@ -42,10 +64,14 @@ typedef struct
 char* packUpload(upload_s* pack);
 char* packDownload(download_s* pack);
 
-upload_s makeUpPack(float x, float y, float z, float yaw);
-download_s makeDownPack(float x, float y, float z, float yaw,
+upload_s makeUpPack(enum CtrlMode ctrlMode, float pitch, float roll,
+    float x, float y, float z, float yaw);
+download_s makeDownPack(enum planArmMode arm, enum planeFlyMode flyMode,
+    float x, float y, float z, float yaw,
     float x_set, float y_set, float z_set, float yaw_set,
-    float pitch, float roll, float dis);
+    float pitch, float roll,
+    float pitch_set, float roll_set,
+    float dis);
 
 bool unpackUp(char* str, upload_s* p);
 bool unpackDown(char* str, download_s* p);
