366111a46d34d33ce02d19d671610a4c1bf70339
feilong <614183595@qq.com>
2017-08-02 20:44:07 +0800

    clear

--- src/mavpi/src/offb_node.cpp
+++ src/mavpi/src/offb_node.cpp
@@ -49,11 +49,6 @@ void dis_state_cb(const sensor_msgs::Range::ConstPtr& msg)
     dis_status = *msg;
 }
 
-// #define PI 3.14
-// #define Rad2Deg(r) ((r) / PI * 180)
-// #define Deg2Reg(d) ((d) / 180 * PI)
-// int                        j = 0;
-// float                      i = 0;
 geometry_msgs::PoseStamped xyzyaw2Position(float x, float y, float z, float yaw)
 {
     geometry_msgs::PoseStamped pose;
@@ -66,20 +61,6 @@ geometry_msgs::PoseStamped xyzyaw2Position(float x, float y, float z, float yaw)
 
     return pose;
 }
-
-/*
-geometry_msgs::PoseStamped rollpitchyaw2Position(float z, float roll, float pitch, float yaw)
-{
-    geometry_msgs::PoseStamped pose;
-
-    pose.pose.position.x = 0;
-    pose.pose.position.y = 0;
-    pose.pose.position.z = z;
-
-    pose.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(Deg2Reg(roll), Deg2Reg(pitch), Deg2Reg(yaw));
-
-    return pose;
-}*/
 
 void rawCtrl(float r, float p, float y, float thrust, ros::NodeHandle& nh)
 {
@@ -95,65 +76,6 @@ void rawCtrl(float r, float p, float y, float thrust, ros::NodeHandle& nh)
 
     local_pos_raw_pub.publish(msg);
 }
-
-// //四元数转欧拉角
-// geometry_msgs::Vector3 quad2eular(geometry_msgs::Quaternion quad)
-// {
-//     geometry_msgs::Vector3 eular;
-//
-// #define quad2eularnum 57.3
-//     eular.x = quad2eularnum * asin(-2 * quad.x * quad.z + 2 * quad.w * quad.y); // pitch
-//     eular.y = quad2eularnum * atan2(2 * quad.y * quad.z + 2 * quad.w * quad.x,
-//                                   -2 * quad.x * quad.x - 2 * quad.y * quad.y + 1); // roll
-//     eular.z = quad2eularnum * atan2(2 * (quad.x * quad.y + quad.w * quad.z),
-//                                   quad.w * quad.w + quad.x * quad.x - quad.y * quad.y - quad.z * quad.z); // yaw
-//
-//     return eular;
-// }
-
-// serial                    serial;
-// geometry_msgs::Quaternion setPos;
-// enum CtrlMode             ctrlMode;
-// float                     pitch = 0, roll = 0;
-// void checkUp(char* s)
-// {
-//     upload_s upc;
-//     if (unpackUp(s, &upc)) {
-//         ctrlMode = (enum CtrlMode)upc.ctrlMode;
-//
-//         setPos.x = (float)upc.x / upc.div;
-//         setPos.y = (float)upc.y / upc.div;
-//         setPos.z = (float)upc.z / upc.div;
-//         setPos.w = (float)upc.yaw / upc.div;
-//
-//         pitch = (float)upc.pitch / upc.div;
-//         roll  = (float)upc.roll / upc.div;
-//         // check data
-//         // r,0,0,0,150,70,100,20,
-//         switch (ctrlMode) {
-//         case Pose:
-//             ROS_INFO("REC CTRL CMD => x: [%f], y: [%f], z: [%f], yaw: [%f]", setPos.x, setPos.y, setPos.z, setPos.w);
-//             break;
-//
-//         case Raw:
-//             ROS_INFO("REC CTRL CMD => r: [%f], p: [%f], yaw: [%f], th: [%f]", roll, pitch, setPos.w, setPos.z);
-//             break;
-//
-//         default:
-//             break;
-//         }
-//     } else {
-//         ROS_WARN("REC CMD ERR!");
-//     }
-// }
-
-// void SerialCallBack(string str)
-// {
-//     ROS_INFO(str.c_str());
-//     char temp[BufferLength];
-//     strcpy(temp, str.c_str());
-//     checkUp(temp);
-// }
 
 void waitRosConnect(ros::Rate& rate)
 {
@@ -177,28 +99,6 @@ void preOffboard(ros::Rate& rate, ros::Publisher& local_pos_pub)
     }
     ROS_INFO("pre offboard fished!");
 }
-
-// void sendStatusUseSerial(geometry_msgs::Vector3 eular)
-// {
-//     download_s dp = makeDownPack(
-//         (current_state.armed) ? Arm : NoArm,
-//         (current_state.mode == "OFFBOARD") ? Stable : OffBoard,
-//         pos_status.pose.position.x, pos_status.pose.position.y,
-//         pos_status.pose.position.z,
-//         eular.z,
-//         msg.Data.setPos.x,
-//         msg.Data.setPos.y,
-//         msg.Data.setPos.z,
-//         msg.Data.setPos.w,
-//         eular.x,
-//         eular.y,
-//         0, //
-//         0, //
-//         dis_status.range);
-//     char* dt = packDownload(&dp);
-//     ROS_INFO("%s",dt);
-//     serial.Write(dt, strlen(dt));
-// }
 
 void modeOffBoard(ros::NodeHandle& nh)
 {
@@ -251,36 +151,6 @@ void armAndModeOffboard(ros::NodeHandle& nh, ros::Time& last_request)
     }
 }
 
-// void checkSerialCmd()
-// {
-//     char   buffer[BUFFER_SIZE];
-//     string buff   = "";
-//     bool   save   = false;
-//     int    length = 0;
-//
-//     // check serial data
-//     int bLength = serial.Read(buffer);
-//
-//     for (int i = 0; i < bLength; i++) {
-//         if (readFilter(buffer + i)) {
-//             if (buffer[i] == 'r' && !save) {
-//                 buff.clear();
-//                 save = true;
-//             } else if (buffer[i] == '\n' && save) {
-//                 buff.push_back(buffer[i]);
-//                 SerialCallBack(buff);
-//                 save = false;
-//                 buff.clear();
-//                 serial.clear();
-//             }
-//
-//             if (save) {
-//                 buff.push_back(buffer[i]);
-//             }
-//         }
-//     }
-// }
-
 char CtrlModeMsg[CtrlMode_max][10] = {
     "NoCtl",
     "Pose",
@@ -319,13 +189,6 @@ int main(int argc, char** argv)
 {
     ROS_INFO("System init ...");
 
-
-// // #define RPI
-// #ifdef RPI
-//     serial.Open("/dev/ttyACM0", 115200, 8, NO, 1);
-// #else
-//     serial.Open("/dev/ttyUSB0", 115200, 8, NO, 1);
-// #endif
     ros::init(argc, argv, "mavpi_node");
     // ros节点
     ros::NodeHandle nh;
